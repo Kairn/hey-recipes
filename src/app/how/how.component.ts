22df8d87c8b70a57e977f2ab80852123958842ee
Eddy Soma <esoma92.dev@gmail.com>
2023-08-23 21:58:45 -0400

    sketch out how component and minor fixes

--- src/app/how/how.component.ts
+++ src/app/how/how.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './how.component.html',
   styleUrls: ['./how.component.scss']
 })
-export class HowComponent {
-
-}
+export class HowComponent { }
