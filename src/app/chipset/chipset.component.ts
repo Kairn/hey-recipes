d5d0a802da310b8c154133c707d4e77ed7105b2d
Eddy Soma <esoma92.dev@gmail.com>
2023-09-02 19:32:39 -0400

    chipset functionalities

--- src/app/chipset/chipset.component.ts
+++ src/app/chipset/chipset.component.ts
@@ -1,27 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
+
+import { Chip } from '../main/main.component';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+const TAG = 'tag';
+const INGREDIENT = 'ingredient';
 
 @Component({
   selector: 'app-chipset',
   templateUrl: './chipset.component.html',
   styleUrls: ['./chipset.component.scss']
 })
-export class ChipsetComponent implements OnInit {
+export class ChipsetComponent implements OnInit, AfterViewInit {
 
+  @Input() type!: string;
+  @Input() all_chips!: Chip[];
+  @Input() filtered_chips!: Chip[];
+
+  title!: string;
+  acLabel!: string;
   acControl = new FormControl('');
-  chipOptions: string[] = ['Chip1', 'Hello my friend', '?'];
-  acFilteredOptions!: Observable<string[]>; // Assigned in `ngOnInit`
+  chipOptions!: string[];
+  acFilteredOptions!: Observable<string[]>;
+
+  @ViewChild('chipInput') chipInput!: ElementRef<HTMLInputElement>;
 
   ngOnInit() {
+    this.title = this.type === TAG ? "Filter by tags" : "Filter by ingredients";
+    this.acLabel = this.type === TAG ? "Tags" : "Ingredients";
     this.acFilteredOptions = this.acControl.valueChanges.pipe(
       startWith(''),
       map(value => this._ac_filter(value || '')),
     );
+    this.chipOptions = this.all_chips.filter(chip => {
+      return !this.filtered_chips.map(fc => fc.id).includes(chip.id);
+    }).map(chip => chip.name);
   }
 
-  removeChip() {
-    console.log('Removed chip test');
+  ngAfterViewInit(): void {
+    // Child is set
+  }
+
+  addChip(event: MatAutocompleteSelectedEvent) {
+    for (let chip of this.all_chips) {
+      if (chip.name === event.option.value) {
+        this.filtered_chips.push(chip);
+        break;
+      }
+    }
+    this._ac_update();
+  }
+
+  removeChip(id: number) {
+    this.filtered_chips = this.filtered_chips.filter(chip => chip.id !== id);
+    this._ac_update();
+  }
+
+  private _ac_update() {
+    this.chipOptions = this.all_chips.filter(chip => {
+      return !this.filtered_chips.map(fc => fc.id).includes(chip.id);
+    }).map(chip => chip.name);
+    this.chipInput.nativeElement.value = '';
+    this.acControl.setValue(null);
   }
 
   private _ac_filter(value: string): string[] {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as data from '../../assets/data.json';
+import * as raw_data from '../../assets/data.json';
+
+export interface Chip {
+  readonly id: number;
+  readonly name: string;
+}
+
+export interface Ingredient {
+  readonly id: number;
+  readonly quantity: number;
+}
+
+export interface Meal {
+  readonly id: number;
+  readonly name: string;
+  readonly picUrl: string;
+  readonly desc: string;
+  readonly tags: number[];
+  readonly ingredients: Ingredient[];
+  readonly amount: number;
+}
 
 @Component({
   selector: 'app-main',
@@ -8,9 +28,24 @@ import * as data from '../../assets/data.json';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  data: any = data;
+
+  private raw_data: any = raw_data;
+
+  all_tags!: Chip[];
+  all_ingredients!: Chip[];
+  meals!: Meal[];
+
+  filtered_tags!: Chip[];
+  filtered_ingredients!: Chip[];
 
   ngOnInit(): void {
-    console.log(this.data['ingredients']);
+    // TODO: Load data from cookies
+
+    this.all_tags = this.raw_data['tags'];
+    this.all_ingredients = this.raw_data['ingredients'];
+    this.meals = this.raw_data['meals'];
+
+    this.filtered_tags = [];
+    this.filtered_ingredients = [];
   }
 }
